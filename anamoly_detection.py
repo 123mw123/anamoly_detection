b3e4e69fc6e70a53f3cc194609c33f62a083bae1
Sai Teja Ramadev <raminenisaiteja123@gmail.com>
2017-08-07 21:49:04 +0530

    Update anamoly_detection.py

--- anamoly_detection.py
+++ anamoly_detection.py
@@ -18,21 +18,24 @@ mu = X.mean(axis=0)
 sigma = X.var(axis=0)
 
 
-
+#finding product of guassion distributions of dataset
 prob = np.zeros((X.shape[0], X.shape[1]))
 prob[:,0] = norm(mu[0], sigma[0]).pdf(X[:,0])
 prob[:,1] = norm(mu[1], sigma[1]).pdf(X[:,1])
 prob_product = np.multiply(prob[:, 0], prob[:, 1])
+
+#cross_validation dataset
 cv_X = data['Xval']
 cv_y = data['yval']
 
+#finding product of guassion distributions of cross_validation dataset
 cv_prob = np.zeros((cv_X.shape[0], cv_X.shape[1]))
 cv_prob[:, 0] = norm(mu[0], sigma[0]).pdf(cv_X[:, 0])
 cv_prob[:, 1] = norm(mu[1], sigma[1]).pdf(cv_X[:, 1])
 
 cv_prob_product = np.multiply(cv_prob[:, 0], cv_prob[:, 1])
 
-
+#findind epsilon and f1_score
 best_epsilon = 0
 f1_max = 0
 f = 0
